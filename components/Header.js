e5eaafb2e8cfb076fe379423dbca410c636fe079
dwhddnjs <per1215@naver.com>
2022-02-12 23:03:22 +0900

    Add Link to home

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
 import headerStyles from "../styles/ComponentsStyle/Header.module.css";
 
 const Header = () => {
   return (
     <header className={headerStyles.header}>
       <div className={headerStyles.container}>
-        <Image src="/Logo.png" width={124} height={38} />
+        <Link href="/">
+          <Image src="/Logo.png" width={124} height={38} />
+        </Link>
         <div className={headerStyles.input}>
           <input type="text" placeholder="상품을 검색해보세요!" />
           <Image src="/search.png" width={28} height={28} />
