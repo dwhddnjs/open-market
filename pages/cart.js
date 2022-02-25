03eebb4d9495fb6cf327cd295e2419535fb81a84
dwhddnjs <per1215@naver.com>
2022-02-25 23:28:44 +0900

    add components data

--- pages/cart.js
+++ pages/cart.js
@@ -19,6 +19,8 @@ function cart() {
         </ul>
         <div className={cartStyles.desc}>
           <CartItem />
+          <CartItem />
+          <CartItem />
           {/* <strong>장바구니에 담긴 상품이 없습니다.</strong>
           <small>원하는 상품을 장바구니에 담에 보세요!</small> */}
         </div>
