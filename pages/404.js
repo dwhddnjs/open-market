783b4cb44d33b4433da171567971db092ee81be7
dwhddnjs <per1215@naver.com>
2022-03-14 21:40:47 +0900

    Add 404page markup

---
+++ pages/404.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Custom404() {
+  return (
+    <div>
+      <img src="" alt="" />
+      <h2>페이지를 찾을 수 없습니다.</h2>
+      <p>
+        페이지가 존재하지 않고나 사용할 수 없는 페이지 입니다. 웹 주소가
+        올바른지 확인해 주세요.
+      </p>
+      <div>
+        <button>메인으로</button>
+        <button>이전 페이지</button>
+      </div>
+    </div>
+  );
+}
+
+export default Custom404;
