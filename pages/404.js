9cda8a40f08295f6d892f68e5c5b1c8e693e6920
dwhddnjs <per1215@naver.com>
2022-03-15 23:50:43 +0900

    Add 404page styling css

--- pages/404.js
+++ pages/404.js
@@ -1,17 +1,21 @@
 import React from "react";
+import Image from "next/image";
+import errorStyles from "../styles/LayoutStyle/404.module.css";
 
 function Custom404() {
   return (
-    <div>
-      <img src="" alt="" />
-      <h2>페이지를 찾을 수 없습니다.</h2>
-      <p>
-        페이지가 존재하지 않고나 사용할 수 없는 페이지 입니다. 웹 주소가
-        올바른지 확인해 주세요.
-      </p>
-      <div>
-        <button>메인으로</button>
-        <button>이전 페이지</button>
+    <div className={errorStyles.container}>
+      <Image src="/404.png" width={276} height={236} />
+      <div className={errorStyles.desc}>
+        <h2>페이지를 찾을 수 없습니다.</h2>
+        <p>
+          페이지가 존재하지 않고나 사용할 수 없는 페이지 입니다.
+          <br /> 웹 주소가 올바른지 확인해 주세요.
+        </p>
+        <div className={errorStyles.btnContainer}>
+          <button>메인으로</button>
+          <button>이전 페이지</button>
+        </div>
       </div>
     </div>
   );
